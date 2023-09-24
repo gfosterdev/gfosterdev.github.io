26ed2a42ee8b8236438e93db4be74136e4635568
George Foster <gfoster.dev@gmail.com>
2023-09-24 16:56:46 +0100

    Toying with the idea of having Wordpress act as a headless CMS

--- res/js/script.js
+++ res/js/script.js
@@ -8,4 +8,16 @@ $(document).ready(function () {
 		$("#menu").removeClass("fa-times");
 		$(".navbar").removeClass("nav-toggle");
 	});
+
+	$("#linkedin").click(function () {
+		var win = window.open(
+			"https://www.linkedin.com/in/george-foster-777360144/",
+			"_blank"
+		);
+		if (win) {
+			win.focus();
+		} else {
+			alert("Please allow popups for this website.");
+		}
+	});
 });
